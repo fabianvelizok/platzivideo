487c8edd320c35af8f337ea38a6cef73f1e31d49
Fabián Veliz <velizfabianhoracio@gmail.com>
2018-05-26 23:57:39 -0300

    Add a basic structure for video-player component

---
+++ src/video-player/container/video-player.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import VideoPlayerLayout from '../presentational/video-player-layout';
+
+class VideoPlayer extends Component {
+  render() {
+    return (
+      <VideoPlayerLayout>
+        VideoPlayer
+      </VideoPlayerLayout>
+    );
+  }
+}
+
+export default VideoPlayer;
