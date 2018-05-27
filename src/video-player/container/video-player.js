adecf2cd0ec1eb62c05afac190be9ff5c288e582
Fabián Veliz <velizfabianhoracio@gmail.com>
2018-05-27 16:15:00 -0300

    Control volume value

--- src/video-player/container/video-player.js
+++ src/video-player/container/video-player.js
@@ -8,12 +8,16 @@ import Controls from '../../video-player-controls/presentational/video-player-co
 import { formatTime } from '../../utils/list';
 import ProgressBar from '../../progress-bar/presentational/progress-bar';
 import Spinner from '../../spinner/presentational/spinner';
+import Volume from '../../volume/presentational/volume';
+
 class VideoPlayer extends Component {
   state = {
     pause: true,
     duration: 0,
     currentTime: 0,
     loading: false,
+    prevVolumeValue: 0,
+    volumeValue: 1,
   };
 
   togglePlay = () => {
@@ -51,6 +55,25 @@ class VideoPlayer extends Component {
     });
   }
 
+  handleVolumeChange = (event) => {
+    const volumeValue = event.target.value;
+
+    this.setState({ volumeValue });
+    this.video.volume = volumeValue;
+  }
+
+  handleVolumeIconClick = (event) => {
+    const newVolumeValue = this.state.volumeValue === 0 ?
+      this.state.prevVolumeValue : 0;
+
+    this.setState({
+      prevVolumeValue: this.state.volumeValue,
+      volumeValue: newVolumeValue,
+    });
+
+    this.video.volume = newVolumeValue;
+  }
+
   render() {
     return (
       <VideoPlayerLayout>
@@ -71,6 +94,11 @@ class VideoPlayer extends Component {
             duration={this.state.duration}
             value={this.state.currentTime}
             handleChange={this.handleProgressChange}
+          />
+          <Volume
+            value={this.state.volumeValue}
+            handleChange={this.handleVolumeChange}
+            handleClick={this.handleVolumeIconClick}
           />
         </Controls>
         <Video
---
+++ src/volume/presentational/volume.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import { Volume as VolumeIcon } from '../../icons/presentational/list';
+import './volume.css';
+
+const Volume = (props) => (
+  <div className="Volume">
+    <button
+      className="VolumeButton"
+      onClick={props.handleClick}
+    >
+      <VolumeIcon
+        color="white"
+        size={25}
+      />
+    </button>
+    <div className="VolumeRange">
+      <input
+        type="range"
+        min={0}
+        max={1}
+        step={0.05}
+        value={props.value}
+        onChange={props.handleChange}
+      />
+    </div>
+  </div>
+);
+
+export default Volume;
