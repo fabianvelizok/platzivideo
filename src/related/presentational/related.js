aace39d2b02a19867fc22a4cb4707122a09e4ff3
Fabián Veliz <velizfabianhoracio@gmail.com>
2018-09-03 01:24:08 -0300

    Update Related component

--- src/related/presentational/related.js
+++ src/related/presentational/related.js
@@ -7,6 +7,22 @@ function Related(props) {
   return (
     <div className="Related">
       <Logo />
+
+      <div className="playlist">
+        <h2 class="playlist-heading">My Playlist</h2>
+        <ol>
+          <li>
+            <a href="#">
+              Item 1
+            </a>
+          </li>
+          <li>
+            <a href="#">
+              Item 2
+            </a>
+          </li>
+        </ol>
+      </div>
     </div>
   );
 }
