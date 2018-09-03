b8edf9c413be8fd29b2ac5b67ab05e11b20e8bca
Fabián Veliz <velizfabianhoracio@gmail.com>
2018-09-03 01:45:01 -0300

    Group media queries

--- postcss.config.js
+++ postcss.config.js
@@ -1,9 +1,10 @@
 module.exports = {
   plugins: [
-    require('cssnano'),
+    require('cssnano'), // TODO: Set only in prod.
     require('postcss-import'),
     require('postcss-preset-env')({
       stage: 0
     }),
+    require('css-mqpacker'),
   ]
 };
