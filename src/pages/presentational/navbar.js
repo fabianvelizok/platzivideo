6c93fe484632735b8fdaeb9f32482d83d9cfb411
Fabián Veliz <velizfabianhoracio@gmail.com>
2018-10-17 22:47:06 -0300

    Add new links to Navbar component

--- src/pages/presentational/navbar.js
+++ src/pages/presentational/navbar.js
@@ -8,7 +8,10 @@ function Navbar() {
   return (
     <ul className="Navbar">
       <li><NavLink to="/" exact activeClassName="activeNavItem">Home</NavLink></li>
-      <li><NavLink to="/about" exact activeClassName="activeNavItem">About</NavLink></li>
+      <li><NavLink to="/videos" activeClassName="activeNavItem">Videos</NavLink></li>
+      <li><NavLink to="/profile" activeClassName="activeNavItem">Profile</NavLink></li>
+      <li><NavLink to="/about" activeClassName="activeNavItem">About</NavLink></li>
+      <li><NavLink to="/contact" activeClassName="activeNavItem">Contact</NavLink></li>
     </ul>
   );
 }
