48e13a3ff71b7fcb86e04a97397bd54f74d3e9f2
Fabián Veliz <velizfabianhoracio@gmail.com>
2018-11-25 00:10:07 -0300

    Refactor Search component and show a prompt if there is data in input search

--- src/components/Search/Search.container.js
+++ src/components/Search/Search.container.js
@@ -5,21 +5,27 @@ import { searchMedia } from 'State/actions';
 import Search from 'Components/Search/Search';
 
 class SearchContainer extends Component {
-  handleRef = element => {
-    this.input = element;
+  state = {
+    value: ''
   }
 
   handleSubmit = event => {
     event.preventDefault();
-    this.props.searchMedia(this.input.value);
+    this.props.searchMedia(this.state.value);
+  }
+
+  handleChange = event => {
+    const { value } = event.target;
+    this.setState({ value });
   }
 
   render() {
     return (
       <div>
         <Search
-          handleRef={this.handleRef}
           handleSubmit={this.handleSubmit}
+          handleChange={this.handleChange}
+          value={this.state.value}
         />
       </div>
     );
--- src/components/Search/Search.js
+++ src/components/Search/Search.js
@@ -1,3 +1,5 @@
+import { Prompt } from 'react-router';
+import PropTypes from 'prop-types';
 import React from 'react';
 
 import './Search.css';
@@ -7,15 +9,30 @@ const Search = props => (
     className="search"
     onSubmit={props.handleSubmit}
   >
+    <Prompt message="Are you sure you want to leave?" when={!!props.value} />
+
     <input
       autoComplete="off"
       className="search__field"
+      defaultValue={props.value}
       name="search"
+      onChange={props.handleChange}
       placeholder="Search for media"
-      ref={props.handleRef}
       type="text"
     />
   </form>
 );
+
+Search.propTypes = {
+  handleChange: PropTypes.func,
+  handleSubmit: PropTypes.func,
+  value: PropTypes.string
+};
+
+Search.defaultProps = {
+  handleChange: undefined,
+  handleSubmit: undefined,
+  value: undefined
+};
 
 export default Search;
